8e9204454a7b2f2370a05186d770e8eb6288c32f
pixdoet <63037136+pixdoet@users.noreply.github.com>
2021-09-22 12:25:25 +0800

    Preparations for read functions

--- configs.cpp
+++ configs.cpp
@@ -1,7 +1,10 @@
 //configs.cpp
-//put your login data and post here
 #include <string>
 
-std::string c_username = "USERNAME";
-std::string c_password = "PASSWORD";
-std::string c_postcontent = "POST CONTENT";
+//login data
+std::string G_USERNAME = "USERNAME";
+std::string G_PASSWORD = "PASSWORD";
+// std::string c_postcontent = "c bwitter module test 1";
+
+//link for a parsed reader instance. Default cleantalk's(can be removed if not using read function :him:)
+std::string R_INSTANCE = "https://cleantalk.cf/bwitter/instance.php";
