8bee8f9126a171524a3b4da5ccead2c8e00722f2
pixdoet <63037136+pixdoet@users.noreply.github.com>
2021-09-22 12:21:13 +0800

    Preparations for read functions

---
+++ read.cpp
@@ -0,0 +1,12 @@
+//read.cpp wtf am i a f**king idiot
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <curl/curl.h>
+
+#include "configs.cpp"
+//use a cool thing called "pointer" to point to the server instance string(jk no pointer)
+std::string serverLink = R_INSTANCE;
+int readUser(std::string r_username){
+    return 0;
+}
