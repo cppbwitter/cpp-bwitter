6e755c5013d7ec4e29774caf87fcde348cb1bd94
pixdoet <63037136+pixdoet@users.noreply.github.com>
2021-10-28 16:54:58 +0800

    Add instance link

--- configs.cpp
+++ configs.cpp
@@ -1,9 +1,17 @@
 //configs.cpp
 #include <string>
 
+/*
+    Update 28/10/2021:
+    The official bwitter website(bwitter.me) has been down. In place of this,
+    the source code of bwitter has been released. Change the following string to the
+    bwitter instance of your choice (default still points to bwitter.me)
+*/
+std::string G_INSTANCE = "https://bwitter.me";
+
 //login data
-std::string G_USERNAME = "USERNAME";
-std::string G_PASSWORD = "PASSWORD";
+std::string G_USERNAME = "hiew";
+std::string G_PASSWORD = "";
 // std::string c_postcontent = "c bwitter module test 1";
 
 //link for a parsed reader instance. Default cleantalk's(can be removed if not using read function :him:)
